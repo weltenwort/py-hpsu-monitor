862bfa23682844d21bf762249515618a80e7a5df
Felix Stürmer <stuermer@weltenwort.de>
2021-09-11 17:07:26 +0000

    Add write frame support

--- py_hpsu_monitor/elster_protocol/tests/test_create_can_message.py
+++ py_hpsu_monitor/elster_protocol/tests/test_create_can_message.py
@@ -1,7 +1,20 @@
-import can
-
 from ..create_can_message import create_can_message
-from ..elster_frame import ElsterGenericFrame, ElsterReadRequestFrame
+from ..elster_frame import ElsterGenericFrame, ElsterReadRequestFrame, ElsterWriteFrame
+
+
+def test_create_write_can_message():
+    can_message = create_can_message(
+        ElsterWriteFrame(
+            timestamp=1000000000,
+            sender=0x680,
+            receiver=0x180,
+            elster_index=0x0013,
+            value=48,
+        )
+    )
+
+    assert can_message.arbitration_id == 0x680  # type: ignore
+    assert can_message.data == b"\x30\x00\xFA\x00\x13\x00\x30"
 
 
 def test_create_read_request_can_message():
@@ -11,7 +24,7 @@ def test_create_read_request_can_message():
         )
     )
 
-    assert can_message.arbitration_id == 0x680
+    assert can_message.arbitration_id == 0x680  # type: ignore
     assert can_message.data == b"\x31\x00\xFA\x00\x0E"
 
 
@@ -26,5 +39,5 @@ def test_create_generic_can_message():
         )
     )
 
-    assert can_message.arbitration_id == 0x680
+    assert can_message.arbitration_id == 0x680  # type: ignore
     assert can_message.data == b"\x36\x00\xFA\xDE\xAD\xBE\xEF"
